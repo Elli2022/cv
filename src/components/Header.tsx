320cf929fe0d7cba62f9228a7e4d204c583cbf4d
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-12 11:08:05 +0200

    Added Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+    return ( 
+        <div className="bg-gray-100 p-5 text-center">
+        <h1 className="text-3xl font-bold">Eleonora Nocentini Sköldebrink</h1>
+        <p>Wev Developer | React.js | Next.js | Node.js | Express | Redis</p>
+        </div>
+    )
+}
+
+export default Header;
